37b0a6d3f2b102407d51e0ab25e51c76ecbc0a6f
Malaz Elfatih <malazalfateh@ymail.com>
2022-05-16 10:14:19 +0300

    Add files via upload

---
+++ 1.Programming Foundations Fundamentals/Challenge2.py
@@ -0,0 +1,6 @@
+answer = input ("What's my favortite food?")
+if answer == "rice":
+  print ("Yep So amazing")
+else:
+  print("Yuck! That's not it!")
+print("Thanks for playing")
---
+++ 1.Programming Foundations Fundamentals/Challenge3.py
@@ -0,0 +1,7 @@
+def favorite_City(name):
+  print("one of my favorite City is ",name)
+
+favorite_City("Kasala")
+favorite_City("Joba")
+favorite_City("Kasala")
+favorite_City("Joba")
